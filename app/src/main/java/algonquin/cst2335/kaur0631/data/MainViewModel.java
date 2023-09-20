44bd12c68fd48068a302a76222cfde32a92f3b36
Ramandeep Kaur <kaur0631@algonquinlive.com>
2023-09-20 19:33:15 -0400

    Week2 Completed

---
+++ app/src/main/java/algonquin/cst2335/kaur0631/data/MainViewModel.java
@@ -0,0 +1,8 @@
+package algonquin.cst2335.kaur0631.data;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    public MutableLiveData<Boolean> isSelected=new MutableLiveData<>();
+}
