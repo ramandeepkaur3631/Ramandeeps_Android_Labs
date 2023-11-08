915205604fd1d8755b5406ffda287acc651227d7
Ramandeep Kaur <kaur0631@algonquinlive.com>
2023-11-07 19:46:42 -0500

    Week6 Recycler View done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,13 @@ plugins {
 
 android {
     namespace = "algonquin.cst2335.kaur0631"
-    compileSdk = 33
+    compileSdk = 34
+
+    buildFeatures{
+        viewBinding = true
+        dataBinding = true
+    }
+
 
     defaultConfig {
         applicationId = "algonquin.cst2335.kaur0631"
@@ -33,6 +39,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("androidx.recyclerview:recyclerview:1.3.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/algonquin/cst2335/kaur0631/ChatRoomViewModel.java
@@ -0,0 +1,9 @@
+package algonquin.cst2335.kaur0631;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+
+public class ChatRoomViewModel extends ViewModel {
+    public MutableLiveData<ArrayList<ChatMessage>> messages = new MutableLiveData< >();
+}
