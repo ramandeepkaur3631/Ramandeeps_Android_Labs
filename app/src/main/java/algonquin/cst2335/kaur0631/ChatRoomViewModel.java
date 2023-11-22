0a8dd83915f1d9ef1924cd56e2f7884580f77a4f
Ramandeep Kaur <kaur0631@algonquinlive.com>
2023-11-22 14:49:31 -0500

    Week8 done

--- app/src/main/java/algonquin/cst2335/kaur0631/ChatRoomViewModel.java
+++ app/src/main/java/algonquin/cst2335/kaur0631/ChatRoomViewModel.java
@@ -3,7 +3,7 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import java.util.ArrayList;
-
 public class ChatRoomViewModel extends ViewModel {
     public MutableLiveData<ArrayList<ChatMessage>> messages = new MutableLiveData< >();
+    public MutableLiveData<ChatMessage> selectedMessage = new MutableLiveData< >();
 }
---
+++ app/src/main/java/algonquin/cst2335/kaur0631/MessageDetailsFragment.java
@@ -0,0 +1,32 @@
+package algonquin.cst2335.kaur0631;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import algonquin.cst2335.kaur0631.databinding.DetailsLayoutBinding;
+
+public class MessageDetailsFragment extends Fragment {
+    ChatMessage selected;
+
+    public MessageDetailsFragment(){
+
+    }
+    public MessageDetailsFragment(ChatMessage m) {
+        this.selected = m;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        super.onCreateView(inflater,container,savedInstanceState);
+        DetailsLayoutBinding binding = DetailsLayoutBinding.inflate(inflater);
+        binding.messagetext.setText(selected.message);
+        binding.timetext.setText(selected.timeSent);
+        binding.databaseText.setText("Id = "+selected.id);
+        return binding.getRoot();
+    }
+
+    public void displayMessage(ChatMessage newValue) {
+    }
+}
