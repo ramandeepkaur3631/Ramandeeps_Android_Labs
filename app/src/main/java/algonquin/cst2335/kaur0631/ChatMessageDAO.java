6702cfa446ebb1828f61ed6eb42a3ed414f7b4bb
Ramandeep Kaur <kaur0631@algonquinlive.com>
2023-11-22 14:07:44 -0500

    Week7 done

---
+++ app/src/main/java/algonquin/cst2335/kaur0631/ChatMessageDAO.java
@@ -0,0 +1,16 @@
+package algonquin.cst2335.kaur0631;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+@Dao
+public interface ChatMessageDAO {
+    @Insert
+    public long insertMessage(ChatMessage m);
+    @Query("Select * from ChatMessage")
+    public List<ChatMessage> getAllMessages();
+    @Delete
+    void deleteMessage(ChatMessage m);
+}
---
+++ app/src/main/java/algonquin/cst2335/kaur0631/MessageDatabase.java
@@ -0,0 +1,7 @@
+package algonquin.cst2335.kaur0631;
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+@Database(entities = {ChatMessage.class}, version =2)
+public abstract class MessageDatabase extends RoomDatabase {
+    public abstract ChatMessageDAO cmDAO();
+}
